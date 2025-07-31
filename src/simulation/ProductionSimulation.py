7fb05c8e3281a1489767bb9d452e2b4d7193948a
konrad-adamski <konrad.adamski.ka@gmail.com>
2025-07-31 11:51:23 +0200

    .

--- src/simulation/ProductionSimulation.py
+++ src/simulation/ProductionSimulation.py
@@ -22,7 +22,7 @@ class ProductionSimulation:
         self.verbose = verbose
         self.sigma = sigma
         self.shift_length = shift_length
-        self.machines = {}
+        self.machines: Dict[str, Machine] = {}  # e.g M1 -> Machine(self.env, 'M1')
         self.start_time = 0
         self.pause_time = 0
 
@@ -33,16 +33,16 @@ class ProductionSimulation:
         self.controller = None
         self.env = None
 
-    def reload_machines(self):                                  # for new SimPy environment (continue)
+    def _reload_machines(self):                                  # for new SimPy environment (continue)
         for name, old_machine in self.machines.items():
             self.machines[name] = Machine(self.env, name)
 
-    def add_new_machines(self, machines: set):
+    def _add_new_machines(self, machines: set):
         for m in machines:
             if m not in self.machines:
                 self.machines[m] = Machine(self.env, m)
 
-    def job_process(self, job_id: str, job_operations: List[JobWorkflowOperation]):
+    def _job_process(self, job_id: str, job_operations: List[JobWorkflowOperation]):
 
         if job_id in self.job_information_collection:           # for FCFS
             job_info = self.job_information_collection[job_id]
@@ -58,51 +58,52 @@ class ProductionSimulation:
             with machine.request() as req:
                 yield req
                 sim_start = self.env.now
-                self.log_job_started_on_machine(sim_start, job_id, machine)
+                self._log_job_started_on_machine(sim_start, job_id, machine)
 
                 sim_duration = duration_log_normal(op.duration, sigma=self.sigma)
-                self.register_active_operation(job_op=op, sim_start=sim_start, sim_duration=sim_duration)
+                self._register_active_operation(job_op=op, sim_start=sim_start, sim_duration=sim_duration)
 
                 yield self.env.timeout(sim_duration)
                 sim_end = self.env.now
-                self.log_job_finished_on_machine(sim_end, job_id, machine, sim_duration)
+                self._log_job_finished_on_machine(sim_end, job_id, machine, sim_duration)
 
-            self.add_finished_operation(job_op=op, sim_start=sim_start, sim_end=sim_end)
+            self._add_finished_operation(job_op=op, sim_start=sim_start, sim_end=sim_end)
 
-    def resume_operation_process(self, job_op: JobWorkflowOperation):
+    def _resume_operation_process(self, job_op: JobWorkflowOperation):
         remaining_time = max(0, job_op.end_time - self.start_time)
 
         machine = self.machines[job_op.machine]
-        self.log_job_resumed_on_machine(time_stamp=self.env.now, remaining_time=remaining_time, job_op=job_op)
+        self._log_job_resumed_on_machine(time_stamp=self.env.now, remaining_time=remaining_time, job_op=job_op)
 
         with machine.request() as req:
             yield req
             yield self.env.timeout(remaining_time)
             sim_end = self.env.now
-            self.log_job_finished_on_machine(sim_end, job_op.job_id, machine, remaining_time)
-        self.add_finished_operation(job_op=job_op,sim_start=job_op.start_time,sim_end=sim_end,)
+            self._log_job_finished_on_machine(sim_end, job_op.job_id, machine, remaining_time)
+        self._add_finished_operation(job_op=job_op,sim_start=job_op.start_time,sim_end=sim_end,)
 
     def run(
             self, schedule_collection: Optional[JobOperationWorkflowCollection] = None,
             start_time: int = 0, end_time: int | None = None):
+
         self.start_time = start_time
         self.pause_time = end_time
         self.env = simpy.Environment(initial_time=start_time)
-        self.reload_machines()
+        self._reload_machines()
         self.finished_operations_collection = JobOperationWorkflowCollection()
 
         for job_op in self.active_operations.values():
-            self.env.process(self.resume_operation_process(job_op))
+            self.env.process(self._resume_operation_process(job_op))
 
         if schedule_collection is not None:
             machines = schedule_collection.get_unique_machines()
-            self.add_new_machines(machines)
+            self._add_new_machines(machines)
 
             for job_id, operations in schedule_collection.items():
-                self.env.process(self.job_process(job_id, operations))
+                self.env.process(self._job_process(job_id, operations))
 
-        if end_time is not None:
-            self.env.run(until=end_time)
+        if self.pause_time is not None:
+            self.env.run(until=self.pause_time)
         else:
             self.env.run()
 
@@ -118,14 +119,14 @@ class ProductionSimulation:
         end_time = start_time + self.shift_length
         self.run(schedule_collection=schedule_collection, start_time=start_time, end_time=end_time)
 
-    def log_job_started_on_machine(self, time_stamp, job_id, machine):
+    def _log_job_started_on_machine(self, time_stamp, job_id, machine):
         if self.verbose:
             print(f"[{get_time_str(time_stamp)}] Job {job_id} started on {machine.name}")
         if self.controller:
             self.controller.job_started_on_machine(time_stamp, job_id, machine)
             time.sleep(0.05)
 
-    def log_job_finished_on_machine(self, time_stamp, job_id, machine, sim_duration):
+    def _log_job_finished_on_machine(self, time_stamp, job_id, machine, sim_duration):
         if self.verbose:
             print(f"[{get_time_str(time_stamp)}] Job {job_id} finished on {machine.name} "
                   + f"(after {get_duration(sim_duration)})")
@@ -133,7 +134,7 @@ class ProductionSimulation:
             self.controller.job_finished_on_machine(time_stamp, job_id, machine, sim_duration)
             time.sleep(0.14)
 
-    def log_job_resumed_on_machine(self, time_stamp, remaining_time, job_op: JobWorkflowOperation):
+    def _log_job_resumed_on_machine(self, time_stamp, remaining_time, job_op: JobWorkflowOperation):
         if self.verbose:
             print(f"[{get_time_str(time_stamp)}] Job {job_op.job_id}, Operation {job_op.sequence_number} "
               + f"resumed on {job_op.machine} with {get_duration(remaining_time)} left)")
@@ -141,7 +142,7 @@ class ProductionSimulation:
             self.controller.resume()
             time.sleep(0.14)
 
-    def register_active_operation(self, job_op: JobWorkflowOperation, sim_start, sim_duration):
+    def _register_active_operation(self, job_op: JobWorkflowOperation, sim_start, sim_duration):
         updated_op = replace(
             job_op,
             duration=sim_duration,
@@ -151,7 +152,7 @@ class ProductionSimulation:
         self.active_operations[(job_op.job_id, job_op.sequence_number)] = updated_op
 
 
-    def add_finished_operation(self, job_op: JobWorkflowOperation, sim_start, sim_end):
+    def _add_finished_operation(self, job_op: JobWorkflowOperation, sim_start, sim_end):
         updated_op = replace(
             job_op,
             duration=sim_end - sim_start,
@@ -163,12 +164,7 @@ class ProductionSimulation:
 
         self.active_operations.pop((job_op.job_id, job_op.sequence_number), None)
 
-    def get_active_operations(self) -> JobOperationWorkflowCollection:
-        collection = JobOperationWorkflowCollection()
-        for op in self.active_operations.values():
-            collection.add_operation_instance(op)
-        return collection
-
+    # -------------------- SETTER and GETTER --------------------
     def set_active_operations(self, active_operations_collection: JobOperationWorkflowCollection):
         self.active_operations = {}
         for job_id, op_list in active_operations_collection.items():
@@ -176,8 +172,20 @@ class ProductionSimulation:
                 key = (op.job_id, op.sequence_number)
                 self.active_operations[key] = op
 
-    def get_finished_operations(self) -> JobOperationWorkflowCollection:
+    def get_active_operation_collection(self) -> JobOperationWorkflowCollection:
+        collection = JobOperationWorkflowCollection()
+        for op in self.active_operations.values():
+            collection.add_operation_instance(op)
+        return collection
+
+    def get_finished_operation_collection(self) -> JobOperationWorkflowCollection:
         return self.finished_operations_collection
+
+    def get_waiting_operation_collection(self, schedule_collection: JobOperationWorkflowCollection):
+        return JobOperationWorkflowCollection.subtract_by_job_operation_collections(
+            schedule_collection,
+            self.get_finished_operation_collection(), self.get_active_operation_collection()
+        )
 
 
 if __name__ == "__main__":
@@ -196,29 +204,27 @@ if __name__ == "__main__":
     simulation.run(schedule_collection, start_time= 1440, end_time= 2800)
 
     print("\n","---" * 20, "Finished Operations", "---" * 20)
-    finished_operations = simulation.get_finished_operations()
+    finished_operations = simulation.get_finished_operation_collection()
 
     df_finished = finished_operations.to_dataframe()
     print(df_finished.head(5))
 
     print("\n", "---" * 20, "Active Operations", "---" * 20)
-    active_operations = simulation.get_active_operations()
+    active_operations = simulation.get_active_operation_collection()
 
     df_active = active_operations.to_dataframe()
-    print(df_active.head(5))
+    print(df_active.head(20))
 
     print("\n", "---" * 20, "Waiting Operations", "---" * 20)
 
-    waiting_operation = JobOperationWorkflowCollection.subtract_by_job_operation_collections(
-        main = schedule_collection,
-        exclude1=finished_operations,
-        exclude2=active_operations
-    )
+    waiting_operation = simulation.get_waiting_operation_collection(schedule_collection)
+
+
     df_waiting = waiting_operation.to_dataframe()
-    print(df_waiting.head(5))
+    print(df_waiting.head(20))
 
     print("---"*60)
     print(f"\nScheduleOperations count: {len(df_schedule)}")
     print(f"Finished Operations count: {len(df_finished)}")
-    print(f"Active operations count: {len(active_operations)}")
-    print(f"Waiting operations count: {len(waiting_operation)}")
+    print(f"Active operations count: {len(df_active)}")
+    print(f"Waiting operations count: {len(df_waiting)}")
