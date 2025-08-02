cd2547c9d5ecb918c5c2d3feeb05c9394e550a0c
konrad-adamski <konrad.adamski.ka@gmail.com>
2025-08-02 19:27:53 +0200

    .

--- omega/Builder.py
+++ omega/Builder.py
@@ -111,27 +111,104 @@ class ExperimentBuilder:
         else:
             # Gleichverteilte Routing - Wahrscheinlichkeit p = [1.0 / n] * n
             t_a = np.mean(vec_t_b_mmax) / u_b_mmax
-        return round(t_a, 2)
+        return round(t_a, 4)
+
+    @staticmethod
+    def gen_arrivals(
+            mean_interarrival_time: float, size: int, start_time: int = 0,
+            last_arrival_time: int = 1440, random_seed: Optional[int] = 120)-> List[int]:
+
+        if random_seed is not None:
+            np.random.seed(random_seed)
+
+        # 1) Generate exponentially distributed interarrival times
+        interarrival_times = np.random.exponential(scale=mean_interarrival_time, size=size)
+        interarrival_times[0] = start_time
+
+        # 2) Calculate cumulative arrival times
+        arrivals = np.cumsum(interarrival_times)
+        arrivals = np.floor(arrivals).astype(int)
+
+        # 3) Remove values greater than last_arrival_time
+        arrivals = arrivals[arrivals <= last_arrival_time]
+
+        return np.floor(arrivals).astype(int).tolist()
+
+
+    def create_experiment_with_jobs_(
+            self, routings: List[Routing], solver_main_pct: float = 0.5, solver_w_t:int = 10, solver_w_e:int = 2,
+            solver_w_first:int = 1, max_bottleneck_utilization: float = 0.90, sim_sigma: float  = 0.25,
+            shift_count: int = 1, arrival_seed: Optional[int] = 120,
+            job_routing_seed: Optional[int] = 100, verbose: bool = False):
+
+        experiment = Experiment(
+            main_pct=solver_main_pct,
+            w_t=solver_w_t,
+            w_e=solver_w_e,
+            w_first=solver_w_first,
+            max_bottleneck_utilization=max_bottleneck_utilization,
+            sim_sigma=sim_sigma
+        )
+
+        jobs: List[Job] = []
+        last_shift_end = 1440 * (shift_count+1)
+
+        # Compute mean interarrival time based on max bottleneck utilization
+        mean_arrival_time = self.calculate_mean_interarrival_time(
+            routings=routings,
+            u_b_mmax=max_bottleneck_utilization,
+            verbose=verbose
+        )
+
+        approx_size = np.ceil(last_shift_end / mean_arrival_time)
+
+        arrivals = self.gen_arrivals(
+            mean_interarrival_time=mean_arrival_time,
+            size=approx_size,
+            last_arrival_time=last_shift_end,
+            random_seed=arrival_seed
+        )
+
+        a_idx = 0
+        while a_idx < len(arrivals):
+            temp_routings = routings.copy()
+
+            if job_routing_seed is not None:
+                np.random.seed(job_routing_seed+a_idx)
+                np.random.shuffle(temp_routings)
+
+            for i, routing in enumerate(temp_routings):
+                if a_idx >= len(arrivals):
+                    break
+
+                job = Job(id=f"J{experiment.id:03d}-{a_idx:04d}",
+                    routing=routing,
+                    arrival=arrivals[a_idx],
+                    deadline=None,
+                    experiment=experiment
+                )
+                jobs.append(job)
+                a_idx += 1
+
+        return experiment, jobs
+
 
 
 if __name__ == "__main__":
-    # RoutingSource erzeugen
-    routing_source = RoutingSource(name="Testdatensatz")
+    from configs.path_manager import get_path
 
-    # Example with multiple Routings
-    data = {
-        "Routing_ID": ["R1", "R1", "R2", "R2"],
-        "Operation": [10, 20, 10, 20],
-        "Machine": ["M1", "M2", "M3", "M1"],
-        "Processing Time": [5, 10, 7, 14]
-    }
-    dframe_routings = pd.DataFrame(data)
+    # RoutingSource erzeugen
+    routing_source = RoutingSource(name="FT 10x10")
+
+
+    basic_data_path = get_path("data", "basic")
+    df_routings = pd.read_csv(basic_data_path / "ft10_routings.csv")
+
 
     # Routings aus DataFrame erzeugen
-    routings = Routing.from_multiple_routings_dataframe(dframe_routings, source=routing_source)
-
+    routings = Routing.from_multiple_routings_dataframe(df_routings, source=routing_source)
 
     builder = ExperimentBuilder()
 
-    engpass_machine = builder._get_engpassmaschine_from_routings(routings, verbose=True)
-    print(f"Engpassmachine: {engpass_machine}")
+    mean_arrival_time = builder.calculate_mean_interarrival_time(routings, u_b_mmax=0.9, verbose=True)
+    print(f"\nMean interarrival time: {mean_arrival_time}")
