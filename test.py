2a9164c90cd5337ec56c24ccb497bf0ef3053edd
konrad-adamski <konrad.adamski.ka@gmail.com>
2025-08-11 15:37:03 +0200

    .

--- test.py
+++ test.py
@@ -1,20 +1,21 @@
 from fractions import Fraction
 
 
-tardiness_ration = 0.75
+tardiness_ratio = 0.333
 lateness_ratio = 0.75
 
 
-tardiness_frac = Fraction(tardiness_ration)
+tardiness_frac = Fraction(tardiness_ratio).limit_denominator(100)
 
 tardiness = tardiness_frac.numerator
 earliness = tardiness_frac.denominator - tardiness
 
+print(f"Tardiness ratio: {tardiness_ratio},  {tardiness_frac = }")
 print(f"w_t = {tardiness}, w_e = {earliness}")
 
 print("-"*60)
 
-lateness_frac = Fraction(lateness_ratio)
+lateness_frac = Fraction(lateness_ratio).limit_denominator(100)
 
 lateness_factor = lateness_frac.numerator
 dev_factor = lateness_frac.denominator - lateness_factor
@@ -30,7 +31,6 @@ w_e = earliness * lateness_factor
 
 w_dev = amount * dev_factor
 
-
 print(f"w_t = {w_t}, w_e = {w_e}, w_dev = {w_dev}")
 
 total_shift_number =25
@@ -40,5 +40,5 @@ for shift_number in range(1, total_shift_number + 1):
 
 
 
-
+print("="*60)
 
